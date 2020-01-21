244296a195aaa0bc981eda5276b57e43c07131ba
s-tanaka <seito.tanaka.dev@gmail.com>
2020-01-21 09:05:51 +0900

    Add always connection

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,8 +1,21 @@
-browser.runtime.onInstalled.addListener((details) => {
-  console.log('previousVersion', details.previousVersion)
-})
+function connect() {
+  const socket = new WebSocket('ws://localhost:62115');
 
-const socket = new WebSocket('ws://localhost:62115');
-socket.addEventListener('message', function (event) {
-  new Notification(event.data);
-});
+  socket.onopen = () => {
+    chrome.browserAction.enable();
+  };
+
+  socket.onerror = err => {
+    setTimeout(() => connect(), 5000);
+  };
+
+  socket.onclose = () => {
+    chrome.browserAction.disable();
+  };
+
+  socket.onmessage = event => {
+    new Notification(event.data);
+  };
+}
+
+connect();
